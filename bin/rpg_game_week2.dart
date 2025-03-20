2ae7db97ef058eccdef4d7c5435acbcfda4e3750
ysshimx2 <ysshimx2@naver.com>
2025-03-20 14:34:04 +0900

    캐릭터클래스까지 정의

--- bin/rpg_game_week2.dart
+++ bin/rpg_game_week2.dart
@@ -1,3 +1,29 @@
 import 'package:rpg_game_week2/rpg_game_week2.dart' as rpg_game_week2;
 import 'dart:io';
 import 'dart:math';
+
+class Character {
+  String name;
+  int health;
+  int attackPower;
+  int defensePower;
+
+  Character(this.name, this.health, this.attackPower, this.defensePower);
+
+  void attackMonster(Monster monster) {
+    monster.health -= attackPower;
+    if (monster.health < 0) monster.health = 0;
+    print('$name이(가) ${monster.name}에게 $attackPower의 데미지를 입혔습니다.');
+  }
+
+  void defend() {
+    int recoverHealth = (defensePower ~/ 2);
+    health += recoverHealth;
+    print('$name이(가) 방어 태세를 취하여 $recoverHealth 만큼 체력을 얻었었습니다.');
+  }
+
+  void showStatus() {
+    print('$name - 체력: $health, 공격력: $attackPower, 방어력: $defensePower');
+  }
+}
+// 여기까지 캐릭터 클래스에 대한 정의
