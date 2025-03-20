caf156cd143fa48f86f2265951f053991f891cd0
ysshimx2 <ysshimx2@naver.com>
2025-03-20 13:21:06 +0900

    rpg_game_week2 start

---
+++ bin/rpg_game_week2.dart
@@ -0,0 +1,5 @@
+import 'package:rpg_game_week2/rpg_game_week2.dart' as rpg_game_week2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rpg_game_week2.calculate()}!');
+}
---
+++ test/rpg_game_week2_test.dart
@@ -0,0 +1,8 @@
+import 'package:rpg_game_week2/rpg_game_week2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
