62ee3128b98baa959e65f30e860f7f1e95324994
ysshimx2 <ysshimx2@naver.com>
2025-03-20 13:32:37 +0900

    start2

--- bin/rpg_game_week2.dart
+++ bin/rpg_game_week2.dart
@@ -1,5 +1,3 @@
 import 'package:rpg_game_week2/rpg_game_week2.dart' as rpg_game_week2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${rpg_game_week2.calculate()}!');
-}
+import 'dart:io';
+import 'dart:math';
